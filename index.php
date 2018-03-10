c07d98ae494646c0b13278d27a2426eab6c14eb8
akihiroapp <37249342+akihiroapp@users.noreply.github.com>
2018-03-10 16:57:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,5 +10,6 @@ $options = array(
 
 $context = stream_context_create($options);
 $json = file_get_contents($url, false, $context);
-var_dump(json_decode($json, true));
+$character = json_decode($json);
+echo $character[0];
 ?>

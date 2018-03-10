d86314db26cac6f2afc2bd17e02c56ff69b3b951
akihiroapp <37249342+akihiroapp@users.noreply.github.com>
2018-03-10 15:38:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "Hello World!";
+$url = "http://104.18.39.47/apiv2/tv/0/1/0";
+
+$options = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Host: app.playerlatino.net"
+  )
+);
+
+$context = stream_context_create($options);
+$file = file_get_contents($url, false, $context);
+echo $file;
 ?>

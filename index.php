5c9118de1e4aa77244736c812de6d738fcf92350
akihiroapp <37249342+akihiroapp@users.noreply.github.com>
2018-03-10 18:41:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,16 +11,10 @@ $options = array(
 $context = stream_context_create($options);
 $json = file_get_contents($url, false, $context);
 $json_a = json_decode($json);
-   $regex = "((https?|ftp)\:\/\/)?"; // SCHEME 
-    $regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass 
-    $regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP 
-    $regex .= "(\:[0-9]{2,5})?"; // Port 
-    $regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path 
-    $regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query 
-    $regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor 
+$reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
 
 foreach ($json_a as $k => $v) {
-if(preg_match("/^$regex$/i", $v[3])) // `i` flag for case-insensitive
+if(preg_match($reg_exUrl, $v[3])) // `i` flag for case-insensitive
 { 
   echo $v[3] ;
 }else{

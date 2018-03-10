3115781580fc8e171697749dfb0ec637d07d19e3
akihiroapp <37249342+akihiroapp@users.noreply.github.com>
2018-03-10 17:33:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $options = array(
 $context = stream_context_create($options);
 $json = file_get_contents($url, false, $context);
 $data = json_decode($json);
-foreach ($data as $k=> $val) {
+foreach ($data as $val) {
     echo $val[3]
 }
 ?>

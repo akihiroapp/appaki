3876a52dbccb1535781de5ae9546af5baa44aac5
akihiroapp <37249342+akihiroapp@users.noreply.github.com>
2018-03-10 18:34:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,13 +11,18 @@ $options = array(
 $context = stream_context_create($options);
 $json = file_get_contents($url, false, $context);
 $json_a = json_decode($json);
+   $regex = "((https?|ftp)\:\/\/)?"; // SCHEME 
+    $regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass 
+    $regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP 
+    $regex .= "(\:[0-9]{2,5})?"; // Port 
+    $regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path 
+    $regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query 
+    $regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor 
+
 foreach ($json_a as $k => $v) {
-$ou = preg_match('@^(?:http://)?([^/]+)@i', $v[3], $matches)!=FALSE
-if($ou = true)
-  echo $v[2];
-{   
-}else{
- echo $v[3];
-}
+if(preg_match("/^$regex$/i", $v[3])) // `i` flag for case-insensitive
+{ 
+  return true; 
+} 
 }
 ?>

46fe8238312588a862321e65aa6efb20f6904361
akihiroapp <37249342+akihiroapp@users.noreply.github.com>
2018-03-10 15:41:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ $url = "http://104.18.39.47/apiv2/tv/0/1/0";
 $options = array(
   'http'=>array(
     'method'=>"GET",
-    'header'=>"Host: app.playerlatino.net"
+    'header'=>"Host: app.playerlatino.net . Content-Type: application/json"
   )
 );
 
 $context = stream_context_create($options);
-$file = file_get_contents($url, false, $context);
-echo $file;
+$results = file_get_contents($url, false, $context);
+echo json_encode($results, JSON_PRETTY_PRINT);
 ?>

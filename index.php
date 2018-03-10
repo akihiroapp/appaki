e41a0299a094f0c5cc576fc0dbfb1139f04c4740
akihiroapp <37249342+akihiroapp@users.noreply.github.com>
2018-03-10 18:58:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ $json_a = json_decode($json);
 $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
 
 foreach ($json_a as $k => $v) {
-if(!preg_match($reg_exUrl, $v[3])){
-  echo $v[3];
+if(!preg_match($reg_exUrl, $v[3], $data)){
+  echo $data;
 }
 }
 ?>

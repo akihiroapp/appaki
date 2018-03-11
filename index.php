4130ea092591232cd6316b19580c94209520cc35
akihiroapp <37249342+akihiroapp@users.noreply.github.com>
2018-03-11 11:13:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,28 +13,33 @@ $context = stream_context_create($options);
 $json = file_get_contents($url, false, $context);
 $json_a = json_decode($json);
 $reg_exUrl = "/src/i";
+echo "#EXTM3U";
 switch ($aki){
 case "premium":
 foreach ($json_a as $k => $v) {
 if(!preg_match($reg_exUrl, $v[3], $data)){
 }else{
-  echo "<div>" . $v[1] . " - " . "http://104.18.38.47:80/streaming/" .$v[3] . "?ZGtkYwV2YwR1ZwN3ZwR=" . "</div>";
+  echo "#EXTINF:-1 ," . $v[1];
+  echo "http://104.18.38.47:80/streaming/" .$v[3] . "?ZGtkYwV2YwR1ZwN3ZwR=";
 }
 }
 break;
 case "normal":
 foreach ($json_a as $k => $v) {
 if(!preg_match($reg_exUrl, $v[3], $data)){
-  echo "<div>" . $v[1] . " - " . $v[3] . "</div>";
+  echo "#EXTINF:-1 ," . $v[1];
+  echo $v[3];
 }
 }
 break;
 case "all":
 foreach ($json_a as $k => $v) {
 if(!preg_match($reg_exUrl, $v[3], $data)){
-  echo "<div>" . $v[1] . " - " . $v[3] . "</div>";
+  echo "#EXTINF:-1 ," . $v[1];
+  echo "http://104.18.38.47:80/streaming/" .$v[3] . "?ZGtkYwV2YwR1ZwN3ZwR=";
 }else{
-  echo "<div>" . $v[1] . " - " . "http://104.18.38.47:80/streaming/" .$v[3] . "?ZGtkYwV2YwR1ZwN3ZwR=" . "</div>";
+  echo "#EXTINF:-1 ," . $v[1];
+  echo $v[3];
 }
 }
 }

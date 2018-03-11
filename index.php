5fae91a2084a7774e1974a00316d12e2a792d693
akihiroapp <37249342+akihiroapp@users.noreply.github.com>
2018-03-11 11:23:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,22 +13,22 @@ $context = stream_context_create($options);
 $json = file_get_contents($url, false, $context);
 $json_a = json_decode($json);
 $reg_exUrl = "/src/i";
-echo "#EXTM3U";
+echo "#EXTM3U" ."\n";
 switch ($aki){
 case "premium":
 foreach ($json_a as $k => $v) {
 if(!preg_match($reg_exUrl, $v[3], $data)){
 }else{
-  echo "#EXTINF:-1 ," . $v[1];
-  echo "http://104.18.38.47:80/streaming/" .$v[3] . "?ZGtkYwV2YwR1ZwN3ZwR=";
+  echo "#EXTINF:-1 ," . $v[1] ."\n";
+  echo "http://104.18.38.47:80/streaming/" .$v[3] . "?ZGtkYwV2YwR1ZwN3ZwR=" ."\n";
 }
 }
 break;
 case "normal":
 foreach ($json_a as $k => $v) {
 if(!preg_match($reg_exUrl, $v[3], $data)){
-  echo "#EXTINF:-1 ," . $v[1] . "  ";
-  echo $v[3] . "  ";
+  echo "#EXTINF:-1 ," . $v[1] ."\n";
+  echo $v[3] ."\n";
 }
 }
 break;

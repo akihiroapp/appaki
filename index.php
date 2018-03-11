9617614081cde01e8995578bd627f4725964e251
akihiroapp <37249342+akihiroapp@users.noreply.github.com>
2018-03-11 11:03:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-$a = $_GET['premium'];
-$b = $_GET['normal'];
-$c = $_GET['all'];
+$aki = $_GET['aki'];
 $url = "http://104.18.39.47/apiv2/tv/0/1/0";
 
 $options = array(
@@ -14,13 +12,30 @@ $options = array(
 $context = stream_context_create($options);
 $json = file_get_contents($url, false, $context);
 $json_a = json_decode($json);
-$reg_exUrl = "/src/i";
-echo $a;
+$pre = !preg_match("/src/i", $v[3], $data);
+switch ($aki){
+case "premium":
 foreach ($json_a as $k => $v) {
-if(!preg_match($reg_exUrl, $v[3], $data)){
+if($pre){
+}else{
+  echo "<div>" . $v[1] . " - " . "http://104.18.38.47:80/streaming/" .$v[3] . "?ZGtkYwV2YwR1ZwN3ZwR=" . "</div>";
+}
+}
+break;
+case "normal":
+foreach ($json_a as $k => $v) {
+if($pre){
+  echo "<div>" . $v[1] . " - " . $v[3] . "</div>";
+}
+}
+break;
+case "all":
+foreach ($json_a as $k => $v) {
+if($pre){
   echo "<div>" . $v[1] . " - " . $v[3] . "</div>";
 }else{
   echo "<div>" . $v[1] . " - " . "http://104.18.38.47:80/streaming/" .$v[3] . "?ZGtkYwV2YwR1ZwN3ZwR=" . "</div>";
+}
 }
 }
 ?>
